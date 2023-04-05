ef4523147b24ce3e5c5d4ee882a48410fcd5de1d
Steven Kessler <stvnksslr@gmail.com>
2023-04-04 23:06:22 -0400

    bugfix(cleaner regex):

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ async def grog_responder(say):
     await say(random_grog)
 
 
-@app.message(compile(r"(?<![\w\d])mad(?![\w\d])"))
+@app.message(compile(r"\bmad\b"))
 async def mad_reactor(message, client):
     """
     when someone is mad, let them know that they're mad
