4982deae0f836cbe9a422690213b20fec2845b7f
Steven Kessler <stvnksslr@gmail.com>
2023-04-04 22:48:30 -0400

    bugfix(fixing mad detector regex):

--- src/tests/responder_test.py
+++ src/tests/responder_test.py
@@ -1,6 +1,7 @@
 from re import compile
 
-MAD_FINDER = compile("(?<![\w\d])mad(?![\w\d])")
+MAD_FINDER = compile(r"(?<![\w\d])mad(?![\w\d])")
+
 
 def test__mad_reactor_returns_true():
     """
@@ -9,14 +10,15 @@ def test__mad_reactor_returns_true():
     test_sentence = "I was very mad about tiny mans"
 
     is_mad = MAD_FINDER.findall(test_sentence)
-    assert bool(is_mad) is True 
+    assert bool(is_mad) is True
+
 
 def test__mad_reactor_returns_false():
     """
-    This test should test if the letters in the test 
+    This test should test if the letters in the test
     return when in other words
     """
     test_sentence = "I made this myself"
-   
+
     is_mad = MAD_FINDER.findall(test_sentence)
     assert bool(is_mad) is False
