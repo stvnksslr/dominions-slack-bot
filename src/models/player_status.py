a024ef698691cca0e37abe84953a7e1c51c916fd
Steven Kessler <stvnksslr@gmail.com>
2024-01-29 12:00:46 -0500

    chore(turn emoji):

--- src/models/player_status.py
+++ src/models/player_status.py
@@ -4,4 +4,5 @@ from pydantic.main import BaseModel
 class PlayerStatus(BaseModel):
     name: str
     turn_status: str
+    turn_emoji: str
 
--- src/utils/aiohttp_manager.py
+++
@@ -1,3 +0,0 @@
-from aiohttp import TCPConnector
-
-connector_settings = TCPConnector(limit=20)
