b0db8236fe00e93857e529d0759dbb734bfe1093
Selene Xenia <selene.thea.xenia@gmail.com>
2024-03-14 13:51:22 -0700

    Fixed grog response images and added a line

--- src/responses/grog.py
+++ src/responses/grog.py
@@ -10,8 +10,8 @@ grog_response_list = [
     ":madcat:",
     ":dread:",
     "Grogtech is Back",
-    "https://files.slack.com/files-pri/T2959UF1P-F051BGQTELF/grog.png",
-    "https://files.slack.com/files-pri/T2959UF1P-F0533MG0K2M/zsd.png",
+    "https://i.imgur.com/f3zM2w2.jpeg",
+    "https://i.imgur.com/dkAAMk3.png",
     "Where lies the strangling fruit that came from the hand of the sinner I shall bring forth the seeds of the dead to share with the worms that gather in the darkness and surround the world with the power of their lives while from the dimlit halls of other places forms that never were and never could be writhe for the impatience of the few who never saw what could have been.",
     "In the black water with the sun shining at midnight, those fruit shall come ripe and in the darkness of that which is golden shall split open to reveal the revelation of the fatal softness in the earth. The shadows of the abyss are like the petals of a monstrous flower that shall blossom within the skull and expand the mind beyond what any man can bear, but whether it decays under the earth or above on green fields, or out to sea or in the very air, all shall come to revelation, and to revel, in the knowledge of the strangling fruit—and the hand of the sinner shall rejoice, for there is no sin in shadow or in light that the seeds of the dead cannot forgive.",
     "And there shall be in the planting in the shadows a grace and a mercy from which shall blossom dark flowers, and their teeth shall devour and sustain and herald the passing of an age. That which dies shall still know life in death for all that decays is not forgotten and reanimated it shall walk the world in the bliss of not-knowing. And then there shall be a fire that knows the naming of you, and in the presence of the strangling fruit, its dark flame shall acquire every part of you that remains.",
@@ -20,4 +20,5 @@ grog_response_list = [
     "_wet war elephant growl_",
     "Once there were brook trout in the streams in the mountains. You could see them standing in the amber current where the white edges of their fins wimpled softly in the flow. They smelled of moss in your hand. Polished and muscular and torsional.",
     "On their backs were vermiculate patterns that were maps of the world in its becoming. Maps and mazes. Of a thing which could not be put back. Not be made right again. In the deep glens where they lived all things were older than man and they hummed of mystery.",
+    "Marchioness of Grogmondeley",
 ]
