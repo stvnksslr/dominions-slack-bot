d635e9f6bada7f9ad5e51ae057c9a2eee8108fe1
Steven Kessler <stvnksslr@gmail.com>
2024-06-10 21:45:59 -0400

    fix(whitespace): my lazyness from almost a year ago haunts me now, i should have cleaner entry/exits to inputs to sanitize them consistently

--- src/controllers/command_parser.py
+++ src/controllers/command_parser.py
@@ -37,7 +37,10 @@ async def add_game(command_list: list):
     game_details = await fetch_lobby_details(game_name)
     for player in game_details.player_status:
         await Player().create(
-            nation=player.name.strip(), short_name=player.short_name(), turn_status=player.turn_status, game=current_game
+            nation=player.name.strip(),
+            short_name=player.short_name(),
+            turn_status=player.turn_status,
+            game=current_game,
         )
 
     return f"game {game_name} added"
@@ -82,10 +85,13 @@ async def player_command(command_list: list):
 
     game_name = command_list[1]
     existing_game = await Game.filter().filter(name=game_name, active=True).first()
+
     if not existing_game:
         return f"game {game_name} not found"
-    nation_name = command_list[2].lower()
+
+    nation_name = command_list[2].lower().strip()
     player_name = command_list[3]
+
     try:
         await Player.filter(game=existing_game.id, short_name=nation_name).update(player_name=player_name)
         return f"Updated {nation_name} with {player_name} in {game_name}"
