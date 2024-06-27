8926431f53c792cd9793eb4ff2d5accf848f8dd1
Steven Kessler <stvnksslr@gmail.com>
2024-06-27 10:52:03 -0400

    feat(updated tests + minor dependency bump):

--- src/models/db/base.py
+++ src/models/db/base.py
@@ -2,7 +2,7 @@ from tortoise import Model, fields
 
 
 class BaseModel(Model):
-    id = fields.UUIDField(pk=True)
+    id = fields.UUIDField(primary_key=True)
 
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
