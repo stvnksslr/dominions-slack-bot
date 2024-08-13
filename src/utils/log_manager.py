a8f292341b605a51d9e9b27348d0e781c5eeb485
Steven Kessler <stvnksslr@gmail.com>
2024-08-13 15:15:04 -0400

    feat(logger config): adding better configs for loguru and log level defaults

---
+++ src/utils/log_manager.py
@@ -0,0 +1,13 @@
+from os import getenv
+from sys import stderr
+
+from loguru import logger
+
+
+def setup_logger() -> None:
+    # Get log level from environment variable, default to INFO
+    log_level = getenv("LOG_LEVEL", "INFO").upper()
+
+    # Configure logger
+    logger.remove()  # Remove default handler
+    logger.add(sink=stderr, level=log_level)
