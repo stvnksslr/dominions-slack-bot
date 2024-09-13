1b2d986da5b822b575838843f07b92869d3447ba
Steven Kessler <stvnksslr@gmail.com>
2024-09-13 09:44:41 -0400

    bugfix(check): check was using the db instead of the web for details

--- src/commands/check_commands.py
+++ src/commands/check_commands.py
@@ -6,7 +6,7 @@ from .base import Command
 class CheckGameStatusCommand(Command):
     async def execute(self, game_name: str) -> str:
         try:
-            game_details = await get_lobby_details(game_name, use_db=True)
+            game_details = await get_lobby_details(game_name, use_db=False)
             return str(game_details)
         except ValueError as e:
             return str(e)
